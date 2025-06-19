610eaa3cff5fb4a8096123558ed008126dfffb4e
懒猫_ <15640200348@163.com>
2025-06-19 16:13:40 +0800

    优化主界面

--- Java_Fundamental/src/main/java/cn/edu/neu/java_fundamental/controllers/MainViewController.java
+++ Java_Fundamental/src/main/java/cn/edu/neu/java_fundamental/controllers/MainViewController.java
@@ -33,33 +33,11 @@ public class MainViewController {
     private Button personal_btn;
 
     @FXML
-    private Label userNametag;
-
-    @FXML
-    private Label timetag;
-
-    @FXML
-    private Label roletag;
+    private Label personalfiletag;
 
     @FXML
     private void initialize() {
-        LocalDateTime now = LocalDateTime.now();
-        if(now.getHour()<12){
-            timetag.setText("早上好，");
-        }else if(now.getHour()==12){
-            timetag.setText("中午好，");
-        }else if(now.getHour()<17){
-            timetag.setText("下午好，");
-        }else{
-            timetag.setText("晚上好，");
-        }
-        switch(ChineseRoleName(GlobalData.USER_ROLE)){
-            case "管理员" -> roletag.setText("管理员");
-            case "网格员" -> roletag.setText("网格员");
-            case "公众监督员" -> roletag.setText("公众监督员");
-            default -> roletag.setText("未知角色");
-        }
-        userNametag.setText(GlobalData.CURRENT_USER.getName());
+        initTag();
         if (Objects.equals(GlobalData.USER_ROLE, "Administrator")) {
             System.out.println("Loading Administrator View");
             AsideMenu.getChildren().clear();
@@ -75,10 +53,24 @@ public class MainViewController {
                 AsideMenu.getChildren().add(btn);
             }
             GlobalData.mainViewController = this;
-
         }
+    }
 
+    private void initTag() {
+        personalfiletag.setText(getTimeHello()+"，"+ChineseRoleName(GlobalData.USER_ROLE)+" "+GlobalData.CURRENT_USER.getName());
 
+    }
+    private String getTimeHello() {
+        LocalDateTime now = LocalDateTime.now();
+        if(now.getHour() < 12) {
+            return "早上好";
+        } else if (now.getHour() == 12) {
+            return "中午好";
+        } else if (now.getHour() < 18) {
+            return "下午好";
+        } else {
+            return "晚上好";
+        }
     }
 
     public void navigateTo(String fxmlPath) {
